cccb99cdde8729a8fc413793156f08592b492136
Nadia Pillard <ua9ond@yandex.ru>
2016-02-09 23:22:19 +0600

    Js modified and css&js min added

--- js/main.js
+++ js/main.js
@@ -1,27 +1,26 @@
-$(document).ready(function() {
+'use strict';
+
+$(function() {
 	var subscribeLink = $(".subscribe_popup_link");
 	var subscribePopup = $(".subscribe_popup");
 	var subscribeClose = $(".popup_close");
 	var subscribeSuccesPopup = $(".subscribe_popup_succes");
-	var subscribeSuccesLink = $(".submit_succes");
-	var subscribeSuccesClose = $(".popup_close");
 	var emailLink = $(".email_popup_link");
 	var emailPopup = $(".email_popup");
-	var emailClose = $(".popup_close");
 	var emailSuccesPopup = $(".email_popup_succes");
-	var emailSuccesLink = $(".email_succes");
-	var emailSuccesClose = $(".popup_close");
 	var mobileMenuLink = $("div.mobile_menu");
 	var mobileMenu = $(".mobile_menu_popup");
 	var mobileMenuClose = $(".menu_close");
 	var mobileSubscribeLink = $(".mobile_subscribe_link");
 	var mobileEmailLink = $(".mobile_email_link");
-	
-	
+	var emailSuccesClose = $(".popup_close");
+
+
 	/*navigation*/
 	$(window).scroll(function() {
+		var scrollTop = $(this).scrollTop();
 		if ($(window).width() <= 490) {
-			if ($(this).scrollTop() > 100) {
+			if (scrollTop > 100) {
 				$("nav.mobile_nav").animate({'top':'0px'},400);
 			}
 			else {
@@ -29,7 +28,7 @@ $(document).ready(function() {
 			}
 		}
 		else {
-			if ($(this).scrollTop() > 200) {
+			if (scrollTop > 200) {
 				$("nav.main_nav").animate({'top':'0px'},400);
 			}
 			else {
@@ -37,99 +36,65 @@ $(document).ready(function() {
 			}
 		}
 	});
-	
+
 	/*mobile menu*/
 	mobileMenuLink.click ( function() {
-		mobileMenu.css('display','block').animate({'top': '0'}, 600);	
+		mobileMenu.show().animate({'top': '0'}, 600);
 	});
-	
+
 	mobileMenuClose.click( function() {
-	mobileMenu.animate({top: '-500'}, 400,
-					   function(){
-								$(this).css('display','none');
-							}
-						  );
+		mobileMenu.animate({top: '-500'}, 400, function(){
+			$(this).hide();
+		});
 	});
-	
+
 	mobileSubscribeLink.click( function() {
-	mobileMenu.animate({top: '-500'}, 100,
-					   function(){
-								$(this).css('display','none');
-							}
-						  );	   
+		mobileMenu.animate({top: '-500'}, 100, function(){
+			$(this).hide();
+		});
 	});
-	
+
 	mobileEmailLink.click( function() {
-	mobileMenu.animate({top: '-500'}, 100,
-					   function(){
-								$(this).css('display','none');
-							}
-						  );
+		mobileMenu.animate({top: '-500'}, 100, function(){
+			$(this).hide();
+		});
 	});
-	
-	
-      /*Subscribe popup*/
+
+
+	/*Subscribe popup*/
 	subscribeLink.click ( function(event) {
-		 event.preventDefault();
-		subscribePopup.css('display','block').animate({'right': '0'}, 700);	
+		event.preventDefault();
+		subscribePopup.show().animate({'right': '0'}, 700);
 	});
-	
+
 	subscribeClose.click( function() {
-	subscribePopup.animate({right: '-1000'}, 500,
-					   function(){
-								$(this).css('display','none');
-							}
-						  );
+		hidePopup(subscribePopup);
 	});
-	
+
 	$(".subscribe_form").submit(function(){
-		subscribePopup.animate({right: '-1000'}, 500,
-					   function(){
-								$(this).css('display','none');
-							}
-						  );
+		hidePopup(subscribePopup);
 		subscribeSuccesPopup.css('display','block').animate({'right': '0'}, 700);
 	});
-	
-	subscribeSuccesClose.click( function() {
-	subscribeSuccesPopup.animate({right: '-1000'}, 500,
-					   function(){
-								$(this).css('display','none');
-							}
-						  );
-	});
-	
+
 	/*email popup*/
 	emailLink.click ( function(event) {
-		 event.preventDefault();
-		emailPopup.css('display','block').animate({'right': '0'}, 700);	
+		event.preventDefault();
+		emailPopup.show().animate({'right': '0'}, 700);
 	});
-	
-	emailClose.click( function() {
-	emailPopup.animate({right: '-1000'}, 500,
-					   function(){
-								$(this).css('display','none');
-							}
-						  );
-	});
-	
-	$(".email_form").submit(function(){
-		emailPopup.animate({right: '-1000'}, 500,
-					   function(){
-								$(this).css('display','none');
-							}
-						  );
-		emailSuccesPopup.css('display','block').animate({'right': '0'}, 700);
-	});
-	
-	emailSuccesClose.click( function() {
-	emailSuccesPopup.animate({right: '-1000'}, 500,
-					   function(){
-								$(this).css('display','none');
-							}
-						  );
-	});
-	
-	
 
+	$(".email_form").submit(function(){
+		hidePopup(emailPopup);
+		emailSuccesPopup.show().animate({'right': '0'}, 700);
+	});
+	 emailSuccesClose.click( function() {
+		hidePopup(emailPopup);
+	});
+
+	var hidePopup = function (popup) {
+		popup.animate({right: '-1000'}, 500,
+			function(){
+				$(this).hide();
+			}
+		);
+	}
 });
