33f073a49fca67f2686102a4d2f82956ceb9f818
Nadia Pillard <ua9ond@yandex.ru>
2016-05-07 16:34:06 +0600

    js changed

--- js/video.js
+++ js/video.js
@@ -2,16 +2,4 @@
 
 (function ($) {
   $(".player").mb_YTPlayer();
-  
-    fullscreen();
-    $(window).resize(fullscreen);
-
-    function fullscreen() {
-        var masthead = $('.to-intro');
-        var windowH = $(window).height();
-        var windowW = $(window).width();
-
-        masthead.width(windowW);
-        masthead.height(windowH);
-    }
 }(jQuery));
