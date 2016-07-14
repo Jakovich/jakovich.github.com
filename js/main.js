84d3fbcff3a16d4fe8dc6f47eeb21f340db4670b
Nadia Pillard <ua9ond@yandex.ru>
2016-07-14 17:35:52 +0600

    remontstroy

--- js/main.js
+++ js/main.js
@@ -1,60 +1,17 @@
-$(document).ready(function() {
-  
-  /*функция смены картинок двери*/
-  $('.doors-item__color-btn').click(function(){
-    $currentButton = $(this);
-    var $currentSrc = $currentButton.attr('data-src');
-    var $currentImg = $currentButton.parents('.doors-item').find('img');
-    var imageReplace = new Image();
-    imageReplace.onload = function() {
-      $currentImg.attr('src', $currentSrc);
-    }
-    $currentImg.attr('src', 'img/ajax-loader.gif');
-    imageReplace.src = $currentSrc;
-  })
-  
-  /*функция слайдера*/
-  function htmSlider(){
-    
-    var slideWrap = $('.doors-items__wrapper');
-    
-    var nextLink = $('.doors-slider__nav--prev');
-    
-    var prevLink = $('.doors-slider__nav--next');
 
-    var is_animate = false;
-    
-    var slideWidth = $('.doors-item').outerWidth();
-  
-    var scrollSlider = slideWrap.position().left - slideWidth;
-	 	
- 
-    nextLink.click(function(){
-      if(!slideWrap.is(':animated')) {
-      slideWrap.animate({left: scrollSlider}, 500, function(){
-      slideWrap
-        .find('.doors-item:first')
-        .appendTo(slideWrap)
-        .parent()
-        .css({'left': 0});
-      });
-     }
+"use strict";
+$(document).ready(function(e) {
+	$('.reviews__cboxExample').colorbox({
+      'rel' : 'gal',
+      'maxWidth' : '90%',
+      'current' : ''
     });
-  
-    
-    prevLink.click(function(){
-      if(!slideWrap.is(':animated')) {
-        slideWrap
-       .css({'left': scrollSlider})
-       .find('.doors-item:last')
-       .prependTo(slideWrap)
-       .parent()
-       .animate({left: 0}, 500);
-      }
+    $('.reviews__cboxFeedback').colorbox({
+      'rel' : 'gal2',
+      'maxWidth' : '90%',
+      'current' : ''
     });
- 
-  }
-  
-  htmSlider();
-  
-})
+    $('#input-phone').inputmask("+7 (999) 999-9999", 
+                                { "onincomplete": function(){ alert('inputmask incomplete')}}
+                                 );
+});
