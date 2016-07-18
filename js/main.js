5bc7578690aa8dbc835dffe07886d2fc20e38c0d
Nadia Pillard <ua9ond@yandex.ru>
2016-07-18 14:40:54 +0600

    some changes added

--- js/main.js
+++ js/main.js
@@ -1,36 +1,24 @@
 
 "use strict";
 $(document).ready(function(e) {
-	$('.reviews__cboxExample').colorbox({
-      'rel' : 'gal',
-      'maxWidth' : '95%',
-      'current' : ''
+    $('.popup-report').after("<div class='popup-report__overlay'></div>");
+    $('.popup-report, .popup-report__overlay').hide();
+    $('.basic-btn').click(function(evt){
+      evt.preventDefault();
+      $('.popup-report, .popup-report__overlay').fadeIn(500);
+    })
+    $('.popup-report__close, .popup-report__overlay').click(function(){
+      $('.popup-report, .popup-report__overlay').fadeOut(500);
+    })
+    
+    $(this).keydown(function(eventObject){
+      if (eventObject.which == 27)
+        $('.popup-report, .popup-report__overlay').fadeOut(500);
     });
-    $('.reviews__cboxFeedback').colorbox({
-      'rel' : 'gal2',
-      'maxWidth' : '95%',
-      'current' : ''
-    });
+    
     $('#input-phone').inputmask("+7 (999) 999-9999");
-  
-  // Респонсивный ColorBox
-  jQuery.colorbox.settings.maxWidth  = '95%';
-  jQuery.colorbox.settings.maxHeight = '95%';
-  
-  // Функция ресайза ColorBox
-  var resizeTimer;
-  function resizeColorBox()
-  {
-    if (resizeTimer) clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(function() {
-      if (jQuery('#cboxOverlay').is(':visible')) {
-        jQuery.colorbox.load(true);
-      }
-    }, 300);
-  }
-  
-  // Ресайз при изменении размера окна браузера и
-  // изменении ориентации мобильного устроиства
-  jQuery(window).resize(resizeColorBox);
-  window.addEventListener("orientationchange", resizeColorBox, false);
+    $('#report-phone').inputmask("+7 (999) 999-9999")
+    $('#input-name').inputmask('Regex', { regex: "[A-Za-zА-Яа-яЁё]{3-20}" });
+    $('#report-name').inputmask('Regex', { regex: "[A-Za-zА-Яа-яЁё]{3-20}" });
+   
 });
