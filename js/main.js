1e71b63da6a66cc672065998a921c31566f111ff
Nadia Pillard <ua9ond@yandex.ru>
2016-08-09 16:10:39 +0700

    async links

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     --------------------------------------
   */
   $("head").append("<link href='https://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic&amp;subset=latin,cyrillic-ext' rel='stylesheet' type='text/css'>");
-
+  
   /**
    Открытие и закрытие попапа
    ------------------------------
